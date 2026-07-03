e68250d5dd8f80eb5c2a8ce140fb6cd937982a5c
BL Tan <152669643+boonleongtan@users.noreply.github.com>
2026-07-03 16:01:56 +0800

    style: add NUS logo, add more words fix: link

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import "./App.css";
 import { FaLinkedin } from "react-icons/fa";
+import nusLogo from "./assets/nus-logo.jpg";
 
 function App() {
     return (
         <>
-            <section>
+            <section className="flex items-center gap-4">
+                <div>
+                    <img src={nusLogo} className="h-20" />
+                </div>
                 <div>
                     <h1>Tan Boon Leong</h1>
-                    <p>NUS CEG Year 2</p>
+                    <div className="flex items-center gap-2">
+                        <a href="https://www.linkedin.com/in/tanboonleong">
+                            <FaLinkedin size={30} />
+                        </a>
+                        <p>NUS CEG Year 2</p>
+                    </div>
                 </div>
             </section>
 
             <section className="mt-5">
                 <div>
-                    <a href="www.linkedin.com/in/tanboonleong">
-                        <FaLinkedin size={30} />
-                    </a>
+                    <h3>Projects</h3>
+                    <p>Frontend: React Router v7, Next.js, Expo</p>
+                    <p>Backend: Flask, Firebase, Supabase</p>
+                    <p>Hardware: Arduino, ESP32, ESP8266</p>
                 </div>
             </section>
         </>
